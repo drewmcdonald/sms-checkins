b62bc12efbe54599828f8e2a1706abf4772961a7
drew <drewmcdo@gmail.com>
2019-06-09 15:57:03 -0400

    convert sending to CLI

--- send_basic.py
+++ send_basic.py
@@ -2,15 +2,19 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 from os import getenv
 
+import click
 
-load_dotenv()
 
-
-if __name__ == "__main__":
+@click.command()
+@click.argument("message", required=True)
+def main(message: str) -> None:
     client = Client(getenv("TWILIO_LIVE_ACCOUNT_SID"), getenv("TWILIO_LIVE_AUTH_TOKEN"))
 
     client.messages.create(
-        to=getenv("TO_SMS_NUMBER"),
-        from_=getenv("TWILIO_SMS_NUMBER"),
-        body="Sending another message!",
+        to=getenv("TO_SMS_NUMBER"), from_=getenv("TWILIO_SMS_NUMBER"), body=message
     )
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    main()
